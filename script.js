fd59eb1109ae52cf86412fdd50ebe6ee9d00d1e1
Garrett Sitter <gmsitter@gmail.com>
2016-10-24 20:04:16 -0400

    rebuilt loading feature - now works properly using callback.

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 //API KEY = AIzaSyCC1esCeAIHUljEEc-ZCuoogjxN14vHL_o
 /*
 	TODO
-	1) display intro screen first - minimal buttons for walking, driving
-	2) get user's location -> https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation
-						   -> https://developers.google.com/web/fundamentals/native-hardware/user-location/obtain-location?hl=en
-						   //in chrome 50+ it doesn't work if you aren't using https.  needs to be on a web server for full testing.  Might need to implement this last
 	3) Distance lookup -> https://developers.google.com/maps/documentation/javascript/examples/distance-matrix
 */
 var userLoc;
@@ -21,29 +17,27 @@ var travelTable =
 
 
 
-
-
-
-function initMap(myCallback){
+function initMap(){
 
 	//if browser not supported, display error.
 	if(!navigator.geolocation)
 	{
-		alert("Please enable geolocation to use this service.");
+		alert("Error: Unable to determine your location.  Please enable geolocation to use this service.");
 	}
 	
 	//passed as callback function to navigator.geolocation.getCurrentPosition()
-	function success(){
+	function success(position){
+		
 		//lat and lng will be passed to maps API
 		userLoc = {
 			lat: position.coords.latitude,
 			lng: position.coords.longitude
 		}
-		console.log('Latitude is ' + userLoc.lat + ', Longitude is ' + userLoc.lng + '.');
+		
 	
 		if(userLoc === false)
 		{
-			console.log("Error: Unable to get your location.");
+			alert("Error: Unable to determine your location.  Please enable geolocation to use this service.");
 		}
 		
 		map = new google.maps.Map(document.getElementById('map'), {
@@ -51,13 +45,7 @@ function initMap(myCallback){
 			zoom: 15,
 			disableDefaultUI: true
 		});
-		/*
-		var marker = new google.maps.Marker({
-			position: {lat:35.77, lng:-78.61},
-			map: map,
-			title: 'Click to zoom'
-		});
-		*/
+
 		
 		infowindow = new google.maps.InfoWindow();
 		service = new google.maps.places.PlacesService(map);
@@ -69,6 +57,8 @@ function initMap(myCallback){
 			//minPriceLevel: 0,
 			//maxPriceLevel: 2
 		}
+		$("#map").fadeIn(600);
+
 		service.radarSearch(request, callback);
 	}
 	
@@ -83,11 +73,10 @@ function initMap(myCallback){
 	navigator.geolocation.getCurrentPosition(success, error);
 	///////////////////
 	
-	//execute callback function (used for loading transitions)
-	myCallback();
 }
 
 function callback(results, status) {
+
 	if(status === google.maps.places.PlacesServiceStatus.OK){
 		//get place_id of a random item from the results.
 		var rando = results[Math.floor(Math.random()*results.length)].place_id;
@@ -97,19 +86,16 @@ function callback(results, status) {
 		//use callback function to handle the object returned by the place ID.
 		service.getDetails(detailsRequest, function(place, status){
 			if(status === google.maps.places.PlacesServiceStatus.OK){
-				createMarker(place);
+				//execute marker creation after place had been returned, and after loading GIF has faded out.
+				$(".loader").fadeOut(600, createMarker(place));
 			}
 			console.log(place);
 		});
 
-	
-
-		//create marker from random result.
-		//createMarker();
 	}
 }
 
-function createMarker(place, callback) {
+function createMarker(place) {
 	var placeLoc = place.geometry.location;
 	marker = new google.maps.Marker({
 		map: map,
@@ -156,7 +142,7 @@ function gotoMap(){
 	$("#control").fadeOut(600, function(){
 		console.log("Test");
 		$(".loader").fadeIn(600);
-		initMap(hideLoader);		//pass function to hide loading screen as a callback, execute when finished.
+		initMap();		//pass function to hide loading screen as a callback, execute when finished.
 
 	});
 }
@@ -179,13 +165,13 @@ $(".init").click(function(){
 //hide load screen when map is loaded.
 function hideLoader(){
 	$(".loader").fadeOut(600);
-	$("#map").fadeIn(600);
+	$("#map").fadeIn(600, callback);
 }
 
 //re-display load icon when map is loading.
 function reload(){
 	$('.loader').fadeIn(600, function(){
-		initMap(hideLoader);
+		initMap();
 	});
 }
 
